46b9df6ace04229e1fd7e1687950f72dd36c51f7
zzq120906-web <zzq120906@qq.com>
2026-08-18 12:02:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,187 @@
-// NULL-01 主程序
+// NULL-01 SYSTEM CORE
 
 
-console.log(
-"NULL-01 SYSTEM ONLINE"
+// 启动文字
+
+const text = 
+`
+正在启动 NULL-01 系统...
+
+检查核心文件...
+
+发现异常记录...
+
+恢复协议加载中...
+
+`;
+
+
+
+let index = 0;
+
+
+
+function typeText(){
+
+
+let box =
+document.getElementById("typing");
+
+
+
+if(!box) return;
+
+
+
+if(index < text.length){
+
+
+box.innerHTML += 
+text[index];
+
+
+index++;
+
+
+setTimeout(
+typeText,
+60
 );
 
 
-// 打开第一份日志
-
-function openLog(){
-
-
-    localStorage.setItem(
-        "NULL01_progress",
-        "LOG-000"
-    );
-
-
-    window.location.href =
-    "pages/log.html";
+}
 
 
 }
 
 
 
-// 检查玩家进度
-
-function checkProgress(){
+window.onload=function(){
 
 
-    let progress =
-    localStorage.getItem(
-        "NULL01_progress"
-    );
+typeText();
 
 
-    return progress;
+};
+
+
+
+
+
+// 进入系统
+
+
+function startGame(){
+
+
+localStorage.setItem(
+
+"NULL01_PROGRESS",
+
+"START"
+
+);
+
+
+
+window.location.href=
+
+"pages/log.html";
 
 
 }
 
 
 
-// 解锁记录
-
-function unlock(id){
 
 
-    localStorage.setItem(
-        "NULL01_unlock",
-        id
-    );
+// 获取玩家进度
+
+
+function getProgress(){
+
+
+return localStorage.getItem(
+
+"NULL01_PROGRESS"
+
+);
 
 
 }
 
 
 
-// 获取解锁状态
-
-function getUnlock(){
 
 
-    return localStorage.getItem(
-        "NULL01_unlock"
-    );
+// 保存线索
+
+
+function saveClue(clue){
+
+
+let clues =
+
+JSON.parse(
+
+localStorage.getItem(
+"NULL01_CLUES"
+)
+
+|| "[]"
+
+
+);
+
+
+
+if(!clues.includes(clue)){
+
+
+clues.push(clue);
+
+
+
+localStorage.setItem(
+
+"NULL01_CLUES",
+
+JSON.stringify(clues)
+
+);
+
+
+}
+
+
+
+}
+
+
+
+
+// 检查线索
+
+
+function hasClue(clue){
+
+
+let clues =
+
+JSON.parse(
+
+localStorage.getItem(
+"NULL01_CLUES"
+)
+
+|| "[]"
+
+
+);
+
+
+
+return clues.includes(clue);
 
 
 }
