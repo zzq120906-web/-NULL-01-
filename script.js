0f66e6c61beaaccb0e66f10e139fd018c9125e22
zzq120906-web <zzq120906@qq.com>
2026-08-18 12:33:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,37 @@
 // NULL-01 SYSTEM CORE
 
 
-// 启动文字
+window.onload = function(){
 
-const text = 
-`
-正在启动 NULL-01 系统...
+    let box = document.getElementById("typing");
 
-检查核心文件...
+    if(box){
 
-发现异常记录...
-
-恢复协议加载中...
-
-`;
+        let text = 
+        "正在启动 NULL-01 系统...\n\n检查核心文件...\n\n发现异常记录...\n\n恢复协议加载中...";
 
 
-
-let index = 0;
-
+        let i = 0;
 
 
-function typeText(){
+        function type(){
+
+            if(i < text.length){
+
+                box.innerHTML += text[i];
+
+                i++;
+
+                setTimeout(type,60);
+
+            }
+
+        }
 
 
-let box =
-document.getElementById("typing");
+        type();
 
-
-
-if(!box) return;
-
-
-
-if(index < text.length){
-
-
-box.innerHTML += 
-text[index];
-
-
-index++;
-
-
-setTimeout(
-typeText,
-60
-);
-
-
-}
-
-
-}
-
-
-
-window.onload=function(){
-
-
-typeText();
-
+    }
 
 };
 
@@ -68,120 +39,8 @@ typeText();
 
 
 
-// 进入系统
-
-
 function startGame(){
 
-
-localStorage.setItem(
-
-"NULL01_PROGRESS",
-
-"START"
-
-);
-
-
-
-window.location.href=
-
-"pages/log.html";
-
-
-}
-
-
-
-
-
-// 获取玩家进度
-
-
-function getProgress(){
-
-
-return localStorage.getItem(
-
-"NULL01_PROGRESS"
-
-);
-
-
-}
-
-
-
-
-
-// 保存线索
-
-
-function saveClue(clue){
-
-
-let clues =
-
-JSON.parse(
-
-localStorage.getItem(
-"NULL01_CLUES"
-)
-
-|| "[]"
-
-
-);
-
-
-
-if(!clues.includes(clue)){
-
-
-clues.push(clue);
-
-
-
-localStorage.setItem(
-
-"NULL01_CLUES",
-
-JSON.stringify(clues)
-
-);
-
-
-}
-
-
-
-}
-
-
-
-
-// 检查线索
-
-
-function hasClue(clue){
-
-
-let clues =
-
-JSON.parse(
-
-localStorage.getItem(
-"NULL01_CLUES"
-)
-
-|| "[]"
-
-
-);
-
-
-
-return clues.includes(clue);
-
+    window.location.href = "pages/log.html";
 
 }
